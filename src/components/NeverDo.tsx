df606f30b137e88883fbaa7a738a3e1f35606bbe
Rodrigoteodosio <rodrigo_gost@hotmail.com>
2026-01-06 11:53:06 -0300

    Updated NeverDo.tsx

--- src/components/NeverDo.tsx
+++ src/components/NeverDo.tsx
@@ -1,7 +1,13 @@
 function NeverDo() {
   return (
     <section>
-      <h2>NeverDo</h2>
+      <h2>O que você nunca precisa fazer</h2>
+      <ul>
+        <li>Configurar roteador</li>
+        <li>Abrir portas</li>
+        <li>Entender IP, DNS ou VPN</li>
+        <li>Escolher tipo de conexão</li>
+      </ul>
     </section>
   )
 }
