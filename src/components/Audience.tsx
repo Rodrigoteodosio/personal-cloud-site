1460a53dd3732a1187752d5058ce3e2f8cce6d09
Rodrigoteodosio <rodrigo_gost@hotmail.com>
2026-01-06 11:50:57 -0300

    Updated Audience.tsx

--- src/components/Audience.tsx
+++ src/components/Audience.tsx
@@ -1,7 +1,26 @@
 function Audience() {
   return (
     <section>
-      <h2>Audience</h2>
+      <h2>Para quem é</h2>
+      <div>
+        <div>
+          <h3>Para quem é</h3>
+          <ul>
+            <li>Para quem quer simplicidade</li>
+            <li>Para quem quer controle</li>
+            <li>Para quem quer acessar arquivos de qualquer lugar</li>
+            <li>Para quem não quer aprender nada técnico</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Para quem não é</h3>
+          <ul>
+            <li>Para quem quer configurar servidor</li>
+            <li>Para quem quer mexer em rede</li>
+            <li>Para quem quer dezenas de opções e ajustes</li>
+          </ul>
+        </div>
+      </div>
     </section>
   )
 }
