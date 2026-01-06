90cb3697c6ba1b9f1dc3405f7434e80d6e25aa9b
Rodrigoteodosio <rodrigo_gost@hotmail.com>
2026-01-06 12:43:30 -0300

    Added Signup.tsx

---
+++ src/components/Signup.tsx
@@ -0,0 +1,13 @@
+function Signup() {
+  return (
+    <section>
+      <h1>Criar conta</h1>
+      <p>Você poderá conectar seu armazenamento depois.</p>
+      <input type="email" placeholder="Seu email" />
+      <input type="password" placeholder="Sua senha" />
+      <button>Criar conta</button>
+    </section>
+  )
+}
+
+export default Signup
