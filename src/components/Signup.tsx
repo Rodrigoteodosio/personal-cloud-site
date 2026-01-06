e9cd143deacc4a366d75261b5f6647f5893c07ec
Rodrigoteodosio <rodrigo_gost@hotmail.com>
2026-01-06 12:57:15 -0300

    Updated Signup.tsx

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -3,8 +3,16 @@ function Signup() {
     <section>
       <h1>Criar conta</h1>
       <p>Você poderá conectar seu armazenamento depois.</p>
-      <input type="email" placeholder="Seu email" />
-      <input type="password" placeholder="Sua senha" />
+      <input
+        type="email"
+        placeholder="Seu email"
+        style={{ width: '100%', padding: '12px 16px', marginBottom: '16px' }}
+      />
+      <input
+        type="password"
+        placeholder="Sua senha"
+        style={{ width: '100%', padding: '12px 16px', marginBottom: '16px' }}
+      />
       <button>Criar conta</button>
     </section>
   )
